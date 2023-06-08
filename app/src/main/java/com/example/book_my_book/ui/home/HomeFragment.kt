f930c8374bc4dbaeef4e650ae638f2062eee06d0
noah-barberini-cpnv <noah.barberini@cpnv.ch>
2023-06-08 10:26:59 +0200

    Remove the textview title to add it into fragment

--- app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
@@ -16,25 +16,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.homeTitle
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
+    
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
