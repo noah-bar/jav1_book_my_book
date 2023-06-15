cf69958661d082c87a82e23432e5b5029efc9c84
noah-barberini-cpnv <noah.barberini@cpnv.ch>
2023-06-15 12:11:25 +0200

    Add button to switch display mode

--- app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
@@ -1,20 +1,29 @@
 package com.example.book_my_book.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.book_my_book.BookMyBook
-import com.example.book_my_book.databinding.FragmentAddBookBinding
+import com.example.book_my_book.R
 import com.example.book_my_book.databinding.FragmentHomeBinding
 import com.example.book_my_book.models.Book
+
+enum class DisplayMode {
+    All,
+    Rented,
+}
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    private var _displayMode = DisplayMode.All
+    private lateinit var _btnDisplayAll: Button
+    private lateinit var _btnDisplayRented: Button
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,6 +36,12 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        //get view elements
+        _btnDisplayAll = root.findViewById(R.id.btnDisplayAll)
+        _btnDisplayRented = root.findViewById(R.id.btnDisplayRented)
+        // add listener
+        _btnDisplayAll.setOnClickListener { switchDisplayMode(DisplayMode.All) }
+        _btnDisplayRented.setOnClickListener { switchDisplayMode(DisplayMode.Rented) }
 
         val books = getBooks()
         for (book in books) {
@@ -34,6 +49,25 @@ class HomeFragment : Fragment() {
         }
 
         return root
+    }
+
+    private fun switchDisplayMode(displayMode: DisplayMode) {
+        this._displayMode = displayMode
+        when (this._displayMode) {
+            DisplayMode.All -> {
+                _btnDisplayAll.setBackgroundColor(Color.parseColor("#6200ee"))
+                _btnDisplayAll.setTextColor(Color.WHITE)
+                _btnDisplayRented.setBackgroundColor(Color.WHITE)
+                _btnDisplayRented.setTextColor(Color.BLACK)
+            }
+            DisplayMode.Rented -> {
+                _btnDisplayAll.setBackgroundColor(Color.WHITE)
+                _btnDisplayAll.setTextColor(Color.BLACK)
+                _btnDisplayRented.setBackgroundColor(Color.parseColor("#6200ee"))
+                _btnDisplayRented.setTextColor(Color.WHITE)
+            }
+            else -> {}
+        }
     }
 
     private fun getBooks(): List<Book> {
