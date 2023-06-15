e78bb25ba48b63aa8b6972190ad009773afe36a4
noah-barberini-cpnv <noah.barberini@cpnv.ch>
2023-06-15 11:25:58 +0200

    Add bottom buttons to home page

--- app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/book_my_book/ui/home/HomeFragment.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.book_my_book.BookMyBook
+import com.example.book_my_book.databinding.FragmentAddBookBinding
 import com.example.book_my_book.databinding.FragmentHomeBinding
+import com.example.book_my_book.models.Book
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +19,26 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val books = getBooks()
+        for (book in books) {
+            println(book.title)
+        }
+
+        return root
+    }
+
+    private fun getBooks(): List<Book> {
+        return BookMyBook.db.bookDao().getAll()
+    }
     
     override fun onDestroyView() {
         super.onDestroyView()
